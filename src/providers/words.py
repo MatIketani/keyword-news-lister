875458a0879a541ef9a2e75ff0e227490f1053c0
MatIketani <65353228+MatIketani@users.noreply.github.com>
2022-08-11 19:40:19 -0300

    refactor(words): removed spacy and now comparing keywords and the description

--- src/providers/words.py
+++ src/providers/words.py
@@ -1,51 +1,16 @@
-import spacy
-
-import random
-from string import punctuation
-
-# #
-# 
-# Spacy configuration
-# 
-# #
-
-language = spacy.load("pt_core_news_sm")
-
-# #
-#
-# Get keywords from a specific text
-# 
-# #
-
-def getKeywords(content: str) -> list:
-  result = []
-  word_classes = ["PROPN", "ADJ", "NOUN"]
-  doc = language(content.lower())
-
-  for token in doc:   
-    if(token.text in language.Defaults.stop_words or token.text in punctuation):
-      continue
-
-    if(token.pos_ in word_classes):
-      result.append(token.text)
-
-  return list(set(result))
-
-
 # #
 #
 # Compare keywords lists
 #
 # #
 
-def compareLists(x: list, y: list):
+def compareKeywordsAndText(keywords: list, content: list):
   count = 1
 
-  for w in x:
-    if w in y:
+  content = content.lower().split(" ")
+
+  for w in content:
+    if(w in keywords):
       count = count + 1
 
-  if (len(x) / count) >= 5:
-    return True
-  
-  return False
+  return count / len(keywords)
