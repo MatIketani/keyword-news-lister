cbfe37401defb1704332666551be956b14fbeb24
MatIketani <65353228+MatIketani@users.noreply.github.com>
2022-08-20 22:38:49 -0300

    fix(news): fixed typo on matching percenteage conditional

--- src/providers/news.py
+++ src/providers/news.py
@@ -19,7 +19,7 @@ def listNews(genre: int, keywords: list, isState: bool):
 
     matchingPercentage = words.compareKeywordsAndText(keywords, description)
 
-    if(matchingPercentage > 0.):
+    if(matchingPercentage > 0.8):
       print("[REQUEST FOUND] " + link)
     else:
       pass
